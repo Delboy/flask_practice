230adb8d0e3fd6b7544f523f3e1d5426a4841e98
Delboy <Derek.degoey@hotmail.co.uk>
2022-01-12 15:33:46 +0000

    database migrated

--- recipes/models.py
+++ recipes/models.py
@@ -18,11 +18,11 @@ class Recipe(db.Model):
     # schema for the Recipe model
     id = db.Column(db.Integer, primary_key=True)
     recipe_name = db.Column(db.String(50), unique=True, nullable=False)
-    short_description = db.Column(db.text, nullable=False)
+    short_description = db.Column(db.Text, nullable=False)
     is_vegan = db.Column(db.Boolean, default=False, nullable=False)
     image = db.Column(db.String(200), unique=True, nullable=False)
-    ingredients = db.Column(db.text, nullable=False)
-    method = db.Column(db.text, nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    method = db.Column(db.Text, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
